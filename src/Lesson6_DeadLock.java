ad1036556c3ab6d0d918ce2ef89bb8b8cc931747
Kostya Skrynditsa <k.skrynditsa@teamzone.info>
2020-04-26 12:37:14 +0300

    added hold lock to Dead Lock method

--- src/Lesson6_DeadLock.java
+++ src/Lesson6_DeadLock.java
@@ -13,6 +13,8 @@ public class Lesson6_DeadLock {
         public synchronized void bow(Friend bower) {
             System.out.format("%s: %s has bowed to me!%n",
                     this.name, bower.getName());
+            System.out.format("%s: hold lock " + Thread.holdsLock(this) + "!%n",
+                    this.name);
             bower.bowBack(this);
         }
 
